764b6ed48716477b6f4fabbdaebd5a4c5b47dc8e
Bob <q463250938@gmail.com>
2024-07-15 18:16:42 -0700

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Bo Zhong on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
